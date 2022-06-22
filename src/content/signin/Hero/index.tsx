0921bd970bc2e09c88815518196d4ce5746deb02
fullstack12345 <leo.hhu5731@gmail.com>
2022-06-22 11:57:01 +0900

    create download app view

--- src/content/signin/Hero/index.tsx
+++ src/content/signin/Hero/index.tsx
@@ -1,8 +1,9 @@
 
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom';
-import { Box, Button, Container, Grid, Typography, Link, Stack, LinearProgress } from '@mui/material';
+import { Box, Button, Container, Grid, Typography, Link, Stack, LinearProgress, Fade, Paper } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import ArrowBack from '@mui/icons-material/ArrowBack';
 
 import Logo from 'src/components/LogoSign';
 import StyledButton from 'src/components/StyledButton';
@@ -43,29 +44,31 @@ function Hero() {
         </Link>
       </Box>
       {
-        verifyState===0 && <>
-          <Typography variant='h3' sx={{ my: {xs: 3, sm: 5, md: 7} }}>
-            Hello! 👋<br/>Welcome to Feeds Network!
-          </Typography>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <StyledButton fullWidth onClick={(e)=>{setVerifyState(1)}}>Sign in with DID</StyledButton>
+        verifyState===0 && <Fade in={true}>
+          <Box>
+            <Typography variant='h3' sx={{ my: {xs: 3, sm: 5, md: 7} }}>
+              Hello! 👋<br/>Welcome to Feeds Network!
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <StyledButton fullWidth onClick={(e)=>{setVerifyState(1)}}>Sign in with DID</StyledButton>
+              </Grid>
+              <Grid item xs={12}>
+                <StyledButton type="outline" fullWidth onClick={(e)=>{setVerifyState(3)}}>I don’t have a DID</StyledButton>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant='caption'>
+                  <Link href="https://www.elastos.org/did" underline="always" color='inherit' target="_blank">
+                    What is a DID?
+                  </Link>
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <StyledButton type="outline" fullWidth>I don’t have a DID</StyledButton>
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant='caption'>
-                <Link href="https://www.elastos.org/did" underline="always" color='inherit' target="_blank">
-                  What is a DID?
-                </Link>
-              </Typography>
-            </Grid>
-          </Grid>
-        </>
+          </Box>
+        </Fade>
       }
       {
-        verifyState===1 && <>
+        verifyState===1 && <Fade in={true}>
           <Stack spacing={2} sx={{ mt: {xs: 3, sm: 5, md: 7} }} alignItems='center'>
             <Typography variant='h3' component='div'>
               Authorization
@@ -87,10 +90,10 @@ function Hero() {
               />
             </Box>
           </Stack>
-        </>
+        </Fade>
       }
       {
-        verifyState===2 && <>
+        verifyState===2 && <Fade in={true}>
           <Stack spacing={2} sx={{ mt: {xs: 3, sm: 5, md: 7} }} alignItems='center'>
             <Typography variant='h3' component='div'>
               Authorized
@@ -101,7 +104,35 @@ function Hero() {
             <Box component='img' src='success-check.svg' width={{xs: 80, md: 100, lg: 120}} py={1} draggable={false}/>
             <StyledButton component={RouterLink} to="/home" fullWidth>Let's go!</StyledButton>
           </Stack>
-        </>
+        </Fade>
+      }
+      {
+        verifyState===3 && <Fade in={true}>
+          <Box>
+            <Typography variant='h3' sx={{ my: {xs: 3, sm: 5, md: 7} }}>
+              Download Elastos Essentials app<br/>
+              now to create your own DID!
+            </Typography>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <StyledButton fullWidth href="https://play.google.com/store/apps/details?id=org.elastos.essentials.app" target="_blank">Google Play</StyledButton>
+              </Grid>
+              <Grid item xs={12}>
+                <StyledButton type="outline" fullWidth href="https://apps.apple.com/us/app/elastos-essentials/id1568931743" target="_blank">App Store</StyledButton>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  size="small"
+                  color="inherit"
+                  startIcon={<ArrowBack />}
+                  onClick={()=>{setVerifyState(0)}}
+                >
+                Go back
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Fade>
       }
     </Container>
   );
