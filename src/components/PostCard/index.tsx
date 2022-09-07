3087519fad7f67ffd11a90d51ee0c072a7f16388
fullstack12345 <leo.hhu5731@gmail.com>
2022-09-07 11:46:10 +0900

    add: like and comment UI for post

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Stack, Typography, Card, CardHeader, Divider, lighten, CardActionArea, CardContent, Tooltip, IconButton, Avatar, styled } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Icon } from '@iconify/react';
 
 import StyledAvatar from 'src/components/StyledAvatar'
 import StyledButton from 'src/components/StyledButton'
@@ -48,6 +49,36 @@ const PostCard = (props) => {
               // <Box component='video' src={media.mediaSrc}/>
             ))
           }
+          <svg width={0} height={0}>
+            <linearGradient id="linearColors" x1={0} y1={1} x2={1} y2={1}>
+              <stop offset={0} stopColor="#7624FE" />
+              <stop offset={1} stopColor="#368BFF" />
+            </linearGradient>
+          </svg>
+          <Stack 
+            direction="row" 
+            spacing={2} 
+            sx={{
+              '& svg': {
+                fill: 'url(#linearColors)'
+              },
+              '& svg>path[stroke=currentColor]': {
+                stroke: 'url(#linearColors)'
+              },
+              '& svg>path[fill=currentColor]': {
+                fill: 'unset'
+              }
+            }}
+          >
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <Icon icon="akar-icons:heart" width={18}/>
+              <Typography variant="body2" noWrap>0</Typography>
+            </Stack>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <Icon icon="clarity:chat-bubble-line" width={18}/>
+              <Typography variant="body2" noWrap>0</Typography>
+            </Stack>
+          </Stack>
         </Stack>
       </Box>
     </Card>
