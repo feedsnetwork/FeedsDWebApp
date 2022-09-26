3618b14a50b9ef5bf96aae94f196f8dc47dc9a1d
fullstack12345 <leo.hhu5731@gmail.com>
2022-09-26 21:40:44 +0900

    update add new icons in Credentials page

--- src/content/setting/credentials/index.tsx
+++ src/content/setting/credentials/index.tsx
@@ -6,20 +6,30 @@ import StyledButton from 'src/components/StyledButton';
 import SwitchUI from 'src/components/SwitchUI'
 
 const credentialItems = [
-  {title: 'Avatar', description: 'avatar', id: 'avatar'},
-  {title: 'Name', description: 'name', id: 'name'},
-  {title: 'Description', description: 'description', id: 'description'},
-  {title: 'Website', description: 'website', id: 'website'},
-  {title: 'Pasar', description: 'Pasar account', id: 'pasar'},
-  {title: 'Twitter', description: 'Twitter account', id: 'twitter'},
-  {title: 'Discord', description: 'Discord account', id: 'discord'},
-  {title: 'Telegram', description: 'Telegram account', id: 'telegram'},
-  {title: 'Medium', description: 'Medium account', id: 'medium'},
-  {title: 'KYC-me', description: 'KYC-me badge', id: 'KYC'},
+  {title: 'Avatar', description: 'avatar', id: 'avatar', icon: {name: 'clarity:user-solid'}},
+  {title: 'Name', description: 'name', id: 'name', icon: {name: 'gridicons:nametag'}},
+  {title: 'Description', description: 'description', id: 'description', icon: {name: 'fluent:text-description-24-filled'}},
+  {title: 'Website', description: 'website', id: 'website', icon: {name: 'pajamas:earth'}},
+  {title: 'Pasar', description: 'Pasar account', id: 'pasar', icon: {name: '/static/icons/pasar-badge.svg', type: 'image'}},
+  {title: 'Twitter', description: 'Twitter account', id: 'twitter', icon: {name: 'akar-icons:twitter-fill'}},
+  {title: 'Discord', description: 'Discord account', id: 'discord', icon: {name: 'akar-icons:discord-fill'}},
+  {title: 'Telegram', description: 'Telegram account', id: 'telegram', icon: {name: 'cib:telegram-plane'}},
+  {title: 'Medium', description: 'Medium account', id: 'medium', icon: {name: 'akar-icons:medium-fill'}},
+  {title: 'KYC-me', description: 'KYC-me badge', id: 'KYC', icon: {name: '/static/icons/kyc-badge.svg', type: 'image'}},
 ]
 
 interface CredentialsProps {
   // type?: string;
+}
+const IconInCircle = (props)=>{
+  const {type='icon', name} = props
+  return (
+    type==='icon'?
+    <Box sx={{p: .8, background: (theme)=>theme.palette.primary.main, borderRadius: '50%', display: 'flex', ml: 1, mr: 2}}>
+      <Icon icon={name} width={20} height={20}/>
+    </Box>:
+    <Box component='img' draggable={false} src={name} width={34.4} height={34.4} sx={{ml: 1, mr: 2}}/>
+  )
 }
 const Credentials: FC<CredentialsProps> = (props)=>{
   return (
@@ -34,6 +44,7 @@ const Credentials: FC<CredentialsProps> = (props)=>{
               credentialItems.map((item, _i)=>(
                 <Grid item key={_i}>
                   <Stack direction='row' alignItems='center'>
+                    <IconInCircle {...item.icon}/>
                     <Box flexGrow={1}>
                       <Typography variant='subtitle1'>{item.title}</Typography>
                       <Typography variant='body2' color='text.secondary'>Display {item.description}</Typography>
