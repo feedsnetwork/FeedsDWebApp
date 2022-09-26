4098931f8370df8298275676de0816cf0cb18373
fullstack12345 <leo.hhu5731@gmail.com>
2022-09-26 18:08:34 +0900

    add: post card with image in explorer page update: give layout temporarily in explorer page

---
+++ src/components/PostCard/PostImgCard.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
+import parse from 'html-react-parser';
+import "odometer/themes/odometer-theme-default.css";
+
+import StyledAvatar from 'src/components/StyledAvatar'
+import PaperRecord from 'src/components/PaperRecord'
+import { SidebarContext } from 'src/contexts/SidebarContext';
+import { getDateDistance, isValidTime, hash, convertAutoLink } from 'src/utils/common'
+
+const PostImgCard = (props) => {
+  const { post, contentObj, isReply=false, level=1 } = props
+  const { selfChannels, subscribedChannels, subscriberInfo } = React.useContext(SidebarContext);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isOpenPopover, setOpenPopover] = React.useState(false);
+  const [isEnterPopover, setEnterPopover] = React.useState(false);
+  const currentChannel = [...selfChannels, ...subscribedChannels].find(item=>item.channel_id==post.channel_id) || {}
+  const subscribersOfThis = currentChannel['subscribers'] || []
+  const subscribedByWho = `Subscribed by ${subscribersOfThis.slice(0,3).map(subscriber=>subscriber.display_name).join(', ')}${subscribersOfThis.length>3?' and more!':'.'}`
+
+  const filteredContentByLink = convertAutoLink(contentObj.content)
+  const background = 'url(/temp-img.png) no-repeat center'
+
+  const handlePopper = (e, open)=>{
+    if(open)
+      setAnchorEl(e.target)
+    setOpenPopover(open)
+  }
+
+  return (
+    <PaperRecord sx={{display: 'flex'}}>
+      <Box sx={{ width: '100%', height: '400px', background, backgroundSize: 'cover', position: 'relative'}}>
+        <Stack direction="row" alignItems="center" spacing={2} sx={{position: 'absolute', bottom: 0, p: 1, background: '#161c24c4'}}>
+          <Box
+            // onMouseEnter={(e)=>{handlePopper(e, true)}}
+            // onMouseLeave={(e)=>{handlePopper(e, false)}}
+          >
+            <StyledAvatar alt={contentObj.avatar.name} src={contentObj.avatar.src} width={isReply?40:47}/>
+          </Box>
+          <Box sx={{ minWidth: 0, flexGrow: 1 }}>
+            <Typography 
+              variant="body2" 
+              sx={{
+                whiteSpace: 'pre-line', 
+                '& a.outer-link': {
+                  color: '#368BFF', 
+                  textDecoration: 'none'
+                },
+                lineHeight: 1.3,
+                WebkitLineClamp: 3,
+                WebkitBoxOrient: 'vertical',
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+                display: '-webkit-box !important'
+              }}
+            >
+              {parse(filteredContentByLink)}
+            </Typography>
+          </Box>
+        </Stack>
+      </Box>
+    </PaperRecord>
+  )
+}
+
+export default PostImgCard;
--- src/content/explorer/index.tsx
+++ src/content/explorer/index.tsx
@@ -5,6 +5,7 @@ import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 import TabPanel from 'src/components/TabPanel'
 import ChannelCard from 'src/components/ChannelCard'
 import PostTextCard from 'src/components/PostCard/PostTextCard'
+import PostImgCard from 'src/components/PostCard/PostImgCard'
 import InputOutline from 'src/components/InputOutline'
 import { SidebarContext } from 'src/contexts/SidebarContext';
 
@@ -16,6 +17,7 @@ function Explorer() {
     setTabValue(newValue);
   };
 
+  const tempchannel = {name: 'MMA Lover', avatarImg: '/twitter.png', intro: 'Good weather today in Osaka! Hmm... where should I eat in Tennouji? Any recommendations? I’m thinking of eating raw sushi for the first time though...I hope it’s gonna be alright haha#osaka #japan #spring'}
   const tempost = {created_at: 1664166498000}
   const tempostcontent = {
     content: 'Good weather today in Osaka! Hmm... where should I eat in Tennouji? Any recommendations? I’m thinking of eating raw sushi for the first time though...I hope it’s gonna be alright haha#osaka #japan #spring',
@@ -62,16 +64,42 @@ function Explorer() {
       <TabPanel value={tabValue} index={0} nopadding={true}>
         <Grid container sx={{pt: 2}} spacing={2}>
           <Grid item sm={4} md={3}>
-            <ChannelCard info={{name: 'MMA Lover', avatarImg: '/twitter.png', intro: 'Good weather today in Osaka! Hmm... where should I eat in Tennouji? Any recommendations? I’m thinking of eating raw sushi for the first time though...I hope it’s gonna be alright haha#osaka #japan #spring'}}/>
+            <ChannelCard info={tempchannel}/>
           </Grid>
           <Grid item sm={4} md={3}>
-            <ChannelCard info={{name: 'MMA Lover', avatarImg: '/twitter.png', intro: 'Good weather today in Osaka! Hmm... where should I eat in Tennouji? Any recommendations? I’m thinking of eating raw sushi for the first time though...I hope it’s gonna be alright haha#osaka #japan #spring'}}/>
+            <ChannelCard info={tempchannel}/>
           </Grid>
           <Grid item sm={4} md={3}>
-            <ChannelCard info={{name: 'MMA Lover', avatarImg: '/twitter.png', intro: 'Good weather today in Osaka! Hmm... where should I eat in Tennouji? Any recommendations? I’m thinking of eating raw sushi for the first time though...I hope it’s gonna be alright haha#osaka #japan #spring'}}/>
+            <ChannelCard info={tempchannel}/>
           </Grid>
           <Grid item sm={4} md={3}>
             <PostTextCard post={tempost} contentObj={tempostcontent}/>
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <PostImgCard post={tempost} contentObj={tempostcontent}/>
+              </Grid>
+              <Grid item sm={12} md={6}>
+                <ChannelCard info={tempchannel}/>
+              </Grid>
+              <Grid item sm={12} md={6}>
+                <ChannelCard info={tempchannel}/>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item sm={12} md={6}>
+            <Grid container spacing={2}>
+              <Grid item sm={12} md={6}>
+                <ChannelCard info={tempchannel}/>
+              </Grid>
+              <Grid item sm={12} md={6}>
+                <ChannelCard info={tempchannel}/>
+              </Grid>
+              <Grid item xs={12}>
+                <PostImgCard post={tempost} contentObj={tempostcontent}/>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </TabPanel>
