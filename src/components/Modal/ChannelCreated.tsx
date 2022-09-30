a9cf4cd524382f6aeabf260f80d46b2e83b32354
fullstack12345 <leo.hhu5731@gmail.com>
2022-09-30 09:52:32 +0900

    add: notice modal after channel created

---
+++ src/components/Modal/ChannelCreated.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom'
+import { Dialog, DialogTitle, DialogContent, Typography, Box, Stack } from '@mui/material';
+
+import StyledButton from '../StyledButton';
+import StyledIcon from '../StyledIcon'
+
+function ChannelCreated(props) {
+  const { setOpen, isOpen, setOpenPublish } = props;
+
+  const handleAction = async (e) => {
+    if(e.currentTarget.value==='ok') {
+      setOpenPublish(true)
+    } else {
+      handleClose()
+    }
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onClose={handleClose}>
+      <DialogContent sx={{minWidth: {sm: 'unset', md: 500}}}>
+        <Stack spacing={2} sx={{textAlign: 'center'}}>
+          <Typography variant="h5">Channel Created!</Typography>
+          <StyledIcon icon="bi:check-circle" width={64} height={64}/>
+          <Typography variant="body2">
+            You have created a new channel!<br/>
+            Do you want to make this channel public for other users to discover?
+            You can also publish anytime by accessing the channel preferences.
+          </Typography>
+          <Stack direction="row" spacing={3}>
+            <StyledButton type="outlined" fullWidth value='cancel' onClick={handleAction}>I’ll do it later</StyledButton>
+            <StyledButton fullWidth value='ok' onClick={handleAction}>Let’s do it!</StyledButton>
+          </Stack>
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ChannelCreated
