d34f649a231da988c27ab847285b295a631b1013
fullstack12345 <leo.hhu5731@gmail.com>
2022-10-10 13:36:16 +0900

    :zap: update: implement auto responsive grid of cards in explore page

--- src/content/explore/index.tsx
+++ src/content/explore/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import ReactDOM from "react-dom";
 import { useSelector, useDispatch } from 'react-redux'
 import { Box, Typography, Tabs, Tab, Stack, Container, InputAdornment, Grid } from '@mui/material';
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
+import AutoResponsive from 'autoresponsive-react'
 
 import { CHANNEL_REG_CONTRACT_ABI } from 'src/abi/ChannelRegistry';
 import { ChannelRegContractAddress } from 'src/config';
@@ -19,11 +23,19 @@ import { getIpfsUrl, getWeb3Contract, isJson, getMergedArray, sortByDate } from 
 function Explore() {
   const { selfChannels } = React.useContext(SidebarContext);
   const [tabValue, setTabValue] = React.useState(0);
+  const [containerWidth, setContainerWidth] = React.useState(0);
   const hiveApi = new HiveApi()
   const dispatch = useDispatch()
   const publicChannels = useSelector(selectPublicChannels)
   const publicPosts = useSelector(selectPublicPosts)
   const latestPublicPosts = sortByDate(getMergedArray(publicPosts)).slice(0, 10)
+  const containerRef = React.useRef(null)
+  const theme = useTheme();
+  const greaterThanMid = useMediaQuery(theme.breakpoints.up("md"));
+  const smallToMid = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const lessThanSmall = useMediaQuery(theme.breakpoints.down("sm"));
+  const cardWidthRate = (greaterThanMid && .25) || (smallToMid && .33) || (lessThanSmall && .5) || 1
+  const cardWidthUnit = Math.floor(containerWidth*cardWidthRate/10)*10
   React.useEffect(()=>{
     if(publicChannels.length>0)
       return
@@ -68,7 +80,6 @@ function Explore() {
                             })
                         })
                       })
-                      console.log(tempGroup, "---------pp")
                       dispatch(setPublicPosts(tempGroup))
                     }
                   })
@@ -106,16 +117,30 @@ function Explore() {
                 }
               }
             }
-            console.log(channelInfo, "----------oo")
           })
         }
       })
+    handleResize()
   }, [])
+  
+  const handleResize = () => {
+    setContainerWidth(containerRef.current.clientWidth);
+  }
+  window.addEventListener("resize", handleResize);
 
   const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
     setTabValue(newValue);
   };
-  console.log(publicPosts, "==========pp")
+  const getAutoResponsiveProps = () => {
+    return {
+      itemMargin: 0,
+      containerWidth: containerWidth,
+      itemClassName: "item",
+      transitionDuration: .8,
+      transitionTimingFunction: "easeIn",
+      closeAnimation: false
+    }
+  }
   return (
     <Container sx={{ mt: 3 }} maxWidth={false}>
       <Stack direction="row" spacing={4}>
@@ -149,80 +174,53 @@ function Explore() {
           style={{flexGrow: 1}}
         />
       </Stack>
-      <TabPanel value={tabValue} index={0} nopadding={true}>
-        <Grid container sx={{pt: 2}} spacing={2}>
-          {
-            Object.values(publicChannels).map((channel, _i)=>(
-              <Grid item sm={4} md={3} key={_i}>
-                <ChannelCard info={channel}/>
-              </Grid>
-            ))
-          }
-          {
-            latestPublicPosts.map((post, _i)=>{
-              if(!!post.content.mediaData && post.content.mediaData.length)
-                return (
-                  <Grid item xs={12} md={6} key={_i}>
-                    <PostImgCard post={post}/>
-                  </Grid>
-                )
-              return (
-                <Grid item sm={4} md={3} key={_i}>
-                  <PostTextCard post={post}/>
-                </Grid>
-              )
-            })
-          }
-        </Grid>
-        {/* <Grid container sx={{pt: 2}} spacing={2}>
-          <Grid item sm={4} md={3}>
-            <ChannelCard info={tempchannel}/>
-          </Grid>
-          <Grid item sm={4} md={3}>
-            <ChannelCard info={tempchannel}/>
-          </Grid>
-          <Grid item sm={4} md={3}>
-            <ChannelCard info={tempchannel}/>
-          </Grid>
-          <Grid item sm={4} md={3}>
-            <PostTextCard post={tempost} contentObj={tempostcontent}/>
-          </Grid>
-          <Grid item sm={12} md={6}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <PostImgCard post={tempost} contentObj={tempostcontent}/>
-              </Grid>
-              <Grid item sm={12} md={6}>
-                <ChannelCard info={tempchannel}/>
-              </Grid>
-              <Grid item sm={12} md={6}>
-                <ChannelCard info={tempchannel}/>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item sm={12} md={6}>
-            <Grid container spacing={2}>
-              <Grid item sm={12} md={6}>
-                <ChannelCard info={tempchannel}/>
-              </Grid>
-              <Grid item sm={12} md={6}>
-                <ChannelCard info={tempchannel}/>
-              </Grid>
-              <Grid item xs={12}>
-                <PostImgCard post={tempost} contentObj={tempostcontent}/>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid> */}
-      </TabPanel>
-      <TabPanel value={tabValue} index={1}>
-      </TabPanel>
-      <TabPanel value={tabValue} index={2}>
-      </TabPanel>
-      <TabPanel value={tabValue} index={3}>
-      </TabPanel>
-      <TabPanel value={tabValue} index={4}>
-      </TabPanel>
+      <Box ref={containerRef}>
+        <TabPanel value={tabValue} index={0} nopadding={true}>
+          <Box pt={2}>
+            <AutoResponsive {...getAutoResponsiveProps()}>
+              {
+                Object.values(publicChannels).map((channel, _i)=>{
+                  return <div className="item" style={
+                    {
+                      width: cardWidthUnit,
+                      height: 240,
+                      paddingRight: 8,
+                      paddingBottom: 8,
+                    }
+                  }>
+                    <ChannelCard info={channel} key={_i}/>
+                  </div>
+                })
+              }
+              {
+                latestPublicPosts.map((post, _i)=>{
+                  const isImageCard = !!post.content.mediaData && post.content.mediaData.length
+                  return <div className="item" style={
+                    {
+                      width: isImageCard? 2*cardWidthUnit: cardWidthUnit,
+                      height: isImageCard? 400: 200,
+                      paddingRight: 8,
+                      paddingBottom: 8,
+                    }
+                  }>
+                    {
+                      isImageCard? <PostImgCard post={post}/>: <PostTextCard post={post}/>
+                    }
+                  </div>
+                })
+              }
+            </AutoResponsive>
+          </Box>
+        </TabPanel>
+        <TabPanel value={tabValue} index={1}>
+        </TabPanel>
+        <TabPanel value={tabValue} index={2}>
+        </TabPanel>
+        <TabPanel value={tabValue} index={3}>
+        </TabPanel>
+        <TabPanel value={tabValue} index={4}>
+        </TabPanel>
+      </Box>
     </Container>
   );
 }
