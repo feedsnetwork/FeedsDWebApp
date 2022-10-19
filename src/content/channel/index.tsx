fb7a15cdd1eaaed6e264683eb2c51b14b648040f
fullstack12345 <leo.hhu5731@gmail.com>
2022-10-19 09:33:10 +0900

    update: separate the post form from channel page

--- src/content/channel/index.tsx
+++ src/content/channel/index.tsx
@@ -1,54 +1,23 @@
 import React from 'react'
-import { isString } from 'lodash';
-import { Icon } from '@iconify/react';
 import InfiniteScroll from "react-infinite-scroll-component";
-import { Grid, Container, Box, Typography, Stack, styled, IconButton, Paper } from '@mui/material';
-import { useSnackbar } from 'notistack';
-import closeFill from '@iconify/icons-eva/close-fill';
+import { Grid, Container, Box } from '@mui/material';
 
 import PostCard from 'components/PostCard';
 import { EmptyView } from 'components/EmptyView'
 import { SidebarContext } from 'contexts/SidebarContext';
-import EmojiPopper from 'components/EmojiPopper'
-import StyledButton from 'components/StyledButton';
-import StyledIconButton from 'components/StyledIconButton';
-import StyledTextFieldOutline from 'components/StyledTextFieldOutline'
 import PostSkeleton from 'components/Skeleton/PostSkeleton'
-import { PostContentV3, mediaDataV3, MediaType } from 'models/post_content'
-import { CommonStatus } from 'models/common_content'
-import { reduceDIDstring, getAppPreference, sortByDate, getBufferFromFile, getFilteredArrayByUnique } from 'utils/common'
-import { HiveApi } from 'services/HiveApi'
+import PostBox from './post'
+import { reduceDIDstring } from 'utils/common'
 import { LocalDB, QueryStep } from 'utils/db';
 
-const PostBoxStyle = styled(Box)(({ theme }) => ({
-  position: 'sticky',
-  bottom: 0,
-  zIndex: 1,
-  marginTop: theme.spacing(2),
-  padding: theme.spacing(2),
-  background: 'rgba(22, 28, 36, 0.8)',
-  backdropFilter: 'blur(20px)',
-}));
-
 function Channel() {
-  const { queryStep, focusedChannelId, publishPostNumber, setPublishPostNumber } = React.useContext(SidebarContext);
+  const { queryStep, focusedChannelId, publishPostNumber } = React.useContext(SidebarContext);
   const [posts, setPosts] = React.useState([]);
   const [dispName, setDispName] = React.useState('');
   const [selfChannelCount, setSelfChannelCount] = React.useState(0);
   const [isLoading, setIsLoading] = React.useState(false)
-  const [isOnValidation, setOnValidation] = React.useState(false);
-  const [onProgress, setOnProgress] = React.useState(false);
-  const [postext, setPostext] = React.useState('');
-  const [imageAttach, setImageAttach] = React.useState(null);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [isOpenPopover, setOpenPopover] = React.useState(false);
   const [dispLength, setDispLength] = React.useState(5);
-
-  const { enqueueSnackbar } = useSnackbar();
   const feedsDid = sessionStorage.getItem('FEEDS_DID')
-  const userDid = `did:elastos:${feedsDid}`
-  const hiveApi = new HiveApi()
-  const postRef = React.useRef(null)
 
   React.useEffect(()=>{
     if(queryStep < QueryStep.post_data)
@@ -84,67 +53,6 @@ function Channel() {
     }
   }, [focusedChannelId, publishPostNumber, queryStep])
 
-  const handlePost = async (e) => {
-    setOnValidation(true)
-    if(!postext){
-      postRef.current.focus()
-      return
-    }
-    setOnProgress(true)
-    const postContent = new PostContentV3()
-    postContent.content = postext
-    if(imageAttach) {
-      const imageBuffer = await getBufferFromFile(imageAttach) as Buffer
-      const base64content = imageBuffer.toString('base64')
-      const imageHivePath = await hiveApi.uploadMediaDataWithString(`data:${imageAttach.type};base64,${base64content}`)
-      const tempMediaData: mediaDataV3 = {
-        kind: 'image',
-        originMediaPath: imageHivePath,
-        type: imageAttach.type,
-        size: imageAttach.size,
-        thumbnailPath: imageHivePath,
-        duration: 0,
-        imageIndex: 0,
-        additionalInfo: null,
-        memo: null
-      }
-      postContent.mediaData.push(tempMediaData)
-      postContent.mediaType = MediaType.containsImg
-    }
-    hiveApi.publishPost(focusedChannelId.toString(), "", JSON.stringify(postContent))
-      .then(res=>{
-        // console.log(res, "===============2")
-        enqueueSnackbar('Publish post success', { variant: 'success' });
-        setPublishPostNumber(publishPostNumber+1)
-        setOnProgress(false)
-        setOnValidation(false)
-        setPostext('')
-        setImageAttach(null)
-      })
-  }
-  
-  const handleUploadClick = (e) => {
-    var file = e.target.files[0];
-    console.log(file)
-    setImageAttach(
-      Object.assign(file, {
-        preview: URL.createObjectURL(file)
-      })
-    );
-  }
-  const handleImageAttachRemove = (e) => {
-    setImageAttach(null);
-  };
-  const handleChangePostext = (e) => {
-    setPostext(e.target.value)
-  }
-  const handlePopper = (e)=>{
-    setAnchorEl(e.currentTarget)
-    setOpenPopover(true)
-  }
-  const onEmojiClick = (emojiObject, _) => {
-    setPostext((prev)=>`${prev}${emojiObject.emoji}`);
-  };
   const appendMoreData = () => {
     setDispLength(dispLength+5)
   }
@@ -194,92 +102,7 @@ function Channel() {
                   </Grid>
                 </InfiniteScroll>
               </Container>
-              <PostBoxStyle>
-                <Stack spacing={2}>
-                  <StyledTextFieldOutline
-                    inputRef={postRef}
-                    value={postext}
-                    multiline
-                    minRows={3}
-                    maxRows={10}
-                    placeholder="What's up"
-                    onChange={handleChangePostext}
-                    error={isOnValidation&&!postext}
-                    helperText={isOnValidation&&!postext?'Message is required':''}
-                  />
-                  {
-                    !!imageAttach && 
-                    <Stack>
-                      {
-                        !!imageAttach &&
-                        <Box sx={{
-                          width: 80,
-                          height: 80,
-                          borderRadius: 1,
-                          overflow: 'hidden',
-                          position: 'relative',
-                          display: 'inline-flex'
-                        }}>
-                          <Paper
-                            variant="outlined"
-                            component="img"
-                            src={isString(imageAttach) ? imageAttach : imageAttach.preview}
-                            sx={{ width: '100%', height: '100%', objectFit: 'fill', position: 'absolute' }}
-                          />
-                          <Box sx={{ top: 6, right: 6, position: 'absolute' }}>
-                            <IconButton
-                              size="small"
-                              onClick={handleImageAttachRemove}
-                              sx={{
-                                p: '2px',
-                                color: 'common.white',
-                              }}
-                            >
-                              <Icon icon={closeFill} />
-                            </IconButton>
-                          </Box>
-                        </Box>
-                      }
-                    </Stack>
-                  }
-                  <Stack direction='row'>
-                    <Box sx={{ alignItems: 'center', display: 'flex', flexGrow: 1 }}>
-                      <input
-                        accept="image/*"
-                        id="contained-button-file"
-                        // multiple
-                        type="file"
-                        onChange={handleUploadClick}
-                        style={{display: 'none'}}
-                      />
-                      <label htmlFor="contained-button-file">
-                        <StyledIconButton icon="clarity:picture-line"/>
-                      </label>
-                      <StyledIconButton icon="clarity:camera-line"/>
-                      <StyledIconButton icon="clarity:video-gallery-line"/>
-                      <StyledIconButton icon="clarity:video-camera-line"/>
-                      <StyledIconButton icon="fluent:emoji-laugh-24-regular" onClick={handlePopper}/>
-                      <IconButton>
-                        <Typography variant='body2' sx={{
-                          backgroundImage: 'linear-gradient(90deg, #7624FE 0%, #368BFF 100%)',
-                          backgroundSize: '100%',
-                          backgroundRepeat: 'repeat',
-                          WebkitBackgroundClip: 'text',
-                          WebkitTextFillColor: 'transparent',
-                          MozBackgroundClip: 'text',
-                          MozTextFillColor: 'transparent',
-                        }}>
-                          NFT
-                        </Typography>
-                      </IconButton>
-                    </Box>
-                    <Box width={150}>
-                      <StyledButton fullWidth loading={onProgress} needLoading={true} onClick={handlePost}>Post</StyledButton>
-                    </Box>
-                  </Stack>
-                </Stack>
-                <EmojiPopper {...{anchorEl, isOpenPopover, setOpenPopover, onEmojiClick}}/>
-              </PostBoxStyle>
+              <PostBox/>
             </Box>
           }
         </>
