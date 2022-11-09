e470969f3abd7e40d4addfce767614db80dda3c5
fullstack12345 <leo.hhu5731@gmail.com>
2022-11-09 16:59:14 +0900

    add validation in handling when loading avatar is error

--- src/components/StyledAvatar/index.tsx
+++ src/components/StyledAvatar/index.tsx
@@ -42,7 +42,9 @@ const AvatarBox = styled(Box)(({ theme }) => ({
 const StyledAvatar: FC<StyledAvatarProps> = (props) => {
   const {alt, src, width=47, style={}} = props
   const handleErrorImage = (e) => {
-    e.target.src = '/loading.svg'
+    // e.target.src = '/loading.svg'
+    if(!src.startsWith("http"))
+      return
     fetch(src)
       .then(res=>res.text())
       .then(res=>{
