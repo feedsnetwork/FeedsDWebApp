bff666c97e5300d78b273cff9736e5fa7d885ffc
fullstack12345 <leo.hhu5731@gmail.com>
2022-11-24 18:33:55 +0900

    update comment card using revised user info state

--- src/components/CommentCard/index.tsx
+++ src/components/CommentCard/index.tsx
@@ -3,36 +3,43 @@ import { useSelector } from 'react-redux'
 import { Box, Stack, Card } from '@mui/material';
 
 import PostBody from 'components/PostCard/PostBody'
-import { selectUsers } from 'redux/slices/user';
+import { selectUserInfoByDID } from 'redux/slices/user';
 import { decodeBase64, reduceDIDstring } from 'utils/common'
 import { getLocalDB } from 'utils/db';
 import { selectChannelById } from 'redux/slices/channel';
 
-const CommentCard = (props) => {
-  const { comment, direction='column' } = props
-  const channel = useSelector(selectChannelById(comment.channel_id)) || {}
-  const users = useSelector(selectUsers)
-  const [commentData, setCommentData] = React.useState([])
-  const LocalDB = getLocalDB()
-
-  const commentUser = users[comment.creater_did] || {}
+const getContentObj = (comment, channel, commentUser, parentReplyingTo='') => {
   let dispName = commentUser['name'] || reduceDIDstring(comment.creater_did)
   let dispAvatar = { name: commentUser['name'], src: decodeBase64(commentUser['avatarSrc'])}
   if(channel['target_did'] === comment.creater_did) {
-    dispName = channel['name']
-    dispAvatar = { name: channel['name'], src: decodeBase64(channel['avatarSrc']) }
+    dispName = channel['display_name']
+    dispAvatar = { name: channel['display_name'], src: decodeBase64(channel['avatarSrc']) }
   }
-
   const replyingTo = channel['owner_name'] || reduceDIDstring(channel['target_did'])
   const contentObj = {
     avatar: dispAvatar || {}, 
     primaryName: `@${dispName}`, 
-    secondaryName: <><b>Replying to</b> @{replyingTo}</>, 
+    secondaryName: <><b>Replying to</b> {parentReplyingTo || `@${replyingTo}`}</>, 
     content: comment.content
   }
   if(comment.status === 1)
     contentObj.content = "(post deleted)"
-
+  return contentObj
+}
+const SubCommentPaper = (props) => {
+  const { comment, channel, parentReplyingTo } = props
+  const commentUser = useSelector(selectUserInfoByDID(comment.creater_did)) || {}
+  const contentObj = getContentObj(comment, channel, commentUser, parentReplyingTo)
+  const bodyProps = { post: comment, contentObj, isReply: true, level: 2 }
+  return <PostBody {...bodyProps}/>
+}
+const CommentCard = (props) => {
+  const { comment, direction='column' } = props
+  const channel = useSelector(selectChannelById(comment.channel_id)) || {}
+  const commentUser = useSelector(selectUserInfoByDID(comment.creater_did)) || {}
+  const [commentData, setCommentData] = React.useState([])
+  const LocalDB = getLocalDB()
+  const contentObj = getContentObj(comment, channel, commentUser)
   const BodyProps = { post: comment, contentObj, level: 2, direction }
   
   React.useEffect(()=>{
@@ -57,21 +64,7 @@ const CommentCard = (props) => {
           commentData.length>0 && 
           <Stack pl={3} pt={2} spacing={1}>
             {
-              commentData.map((comment, _i)=>{
-                const commentUser = users[comment.creator_did] || {}
-                const subContentObj = {
-                  avatar: { name: commentUser['name'], src: commentUser['avatarSrc']},
-                  primaryName: `@${commentUser['name'] || reduceDIDstring(comment.creater_did)}`, 
-                  secondaryName: <><b>Replying to</b> @{dispName}</>, 
-                  content: comment.content
-                }
-                if(channel['target_did'] === comment.creater_did) {
-                  subContentObj['avatar'] = { name: channel['display_name'], src: channel['avatarSrc']}
-                  subContentObj['primaryName'] = `@${channel.display_name}`
-                }
-                const subBodyProps = { post: comment, contentObj: subContentObj, isReply: true, level: 2 }
-                return <PostBody {...subBodyProps} key={_i}/>
-              })
+              commentData.map((subComment, _i)=><SubCommentPaper comment={subComment} channel={channel} parentReplyingTo={contentObj.primaryName} key={_i}/>)
             }
           </Stack>
         }
